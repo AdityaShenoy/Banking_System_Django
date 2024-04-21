2d8e0c5d82c984e8993bf713bb12d9c9c597f5d3
Aditya Shenoy <adityashenoy49@gmail.com>
2024-04-21 19:42:22 +0530

    Allowing nulls in sender and receiver for self withdraw and deposit transactions

--- banking_system/models/transaction_model.py
+++ banking_system/models/transaction_model.py
@@ -8,10 +8,16 @@ from .user_model import UserModel
 # Create your models here.
 class TransactionModel(models.Model):
     sender = models.ForeignKey(
-        UserModel, on_delete=models.CASCADE, related_name="sent_transactions"
+        UserModel,
+        on_delete=models.CASCADE,
+        related_name="sent_transactions",
+        null=True,  # for deposit transactions
     )
     receiver = models.ForeignKey(
-        UserModel, on_delete=models.CASCADE, related_name="received_transactions"
+        UserModel,
+        on_delete=models.CASCADE,
+        related_name="received_transactions",
+        null=True,  # For withdraw transactions
     )
     amount = models.IntegerField()
     time = models.DateTimeField(auto_now=True)
