723061604bf8e374300619c491b1a3ebb54d375e
Aditya Shenoy <adityashenoy49@gmail.com>
2024-04-23 22:11:29 +0530

    Refactored the code to generalize the testing of URL resolution

---
+++ banking_system/banking_system/tests/test_urls.py
@@ -0,0 +1,30 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+
+from home.views import HomeView
+from login.views import LoginView
+from dashboard.views import DashboardView
+from deposit.views import DepositView
+from withdraw.views import WithdrawView
+from send_money.views import SendMoneyView
+from transactions.views import TransactionsView
+
+
+class TestUrls(SimpleTestCase):
+    def test_url_resolution(self):
+        url_name_to_class = {
+            "home": HomeView,
+            "login": LoginView,
+            "dashboard": DashboardView,
+            "deposit": DepositView,
+            "withdraw": WithdrawView,
+            "send_money": SendMoneyView,
+            "transactions": TransactionsView,
+        }
+        for url_name, class_ in url_name_to_class.items():
+            url = reverse(url_name)
+            resolved_url = resolve(url)
+            # Adding ignore comment to suppress false positive error
+            # Cannot access member "view_class" for type "function"
+            self.assertEquals(resolved_url.func.view_class, class_)  # type: ignore
