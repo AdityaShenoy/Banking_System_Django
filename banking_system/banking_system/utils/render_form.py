828a92078487d884a511796f5b7fad6488631f61
Aditya Shenoy <adityashenoy49@gmail.com>
2024-04-24 13:44:50 +0530

    Removed typing import and included direct imports

--- banking_system/banking_system/utils/render_form.py
+++ banking_system/banking_system/utils/render_form.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse
-
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from django.http import HttpRequest
-    from django.forms import Form
+from django.http import HttpRequest
+from django.forms import Form
 
 
-def render_form(request: "HttpRequest", title: str, url_name: str, form: "Form"):
+def render_form(request: HttpRequest, title: str, url_name: str, form: Form):
     return render(
         request, "forms.html", {"title": title, "url": reverse(url_name), "form": form}
     )
--- banking_system/dashboard/views.py
+++ banking_system/dashboard/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
-from django.http.response import HttpResponseRedirect
 from django.views import View
+from django.http.request import HttpRequest
+from django.http.response import HttpResponseRedirect
 from django.urls import reverse
-
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from django.http.request import HttpRequest
 
 
 # Create your views here.
 class DashboardView(View):
-    def get(self, request: "HttpRequest"):
-        if request.session.get("user_name"):
-            return render(request, "dashboard.html")
-        return HttpResponseRedirect(reverse("login"))
+    def get(self, request: HttpRequest):
+        if not request.session.get("user_name"):
+            return HttpResponseRedirect(reverse("login"))
+        return render(request, "dashboard.html")
